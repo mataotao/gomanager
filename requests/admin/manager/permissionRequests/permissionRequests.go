adc308255af430107ffb2d2cb66155436880149b
matao <matao@wjmt.me>
2018-08-12 12:39:20 +0800

    '修改permission'

--- requests/admin/manager/permissionRequests/permissionRequests.go
+++ requests/admin/manager/permissionRequests/permissionRequests.go
@@ -6,7 +6,7 @@ import (
 
 type CreateRequest struct {
 	Label         string `json:"label"`
-	IsContainMenu uint8  `json:"is_contain_menu"`
+	IsContainMenu uint8  `json:"iscontainmenu"`
 	Pid           uint64 `json:"pid"`
 	Url           string `json:"url"`
 	Level         uint8  `json:"level"`
@@ -20,7 +20,7 @@ type CreateResponse struct {
 type UpdateRequest struct {
 	Label         string `json:"label" valid:"required"`
 	Sort          uint64 `json:"sort"`
-	IsContainMenu uint8  `json:"is_contain_menu"`
+	IsContainMenu uint8  `json:"iscontainmenu"`
 	Url           string `json:"url"`
 	UpdatedAt     time.Time
 }
