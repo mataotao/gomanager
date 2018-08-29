594713cd1ab879b22bc595b47e6258144e35be2c
matao <matao@wjmt.me>
2018-08-29 11:10:35 +0800

    clear

--- model/model.go
+++ model/model.go
@@ -11,19 +11,4 @@ type BaseModel struct {
 	//DeletedAt *time.Time `gorm:"column:deleted_at" sql:"index" json:"-"`
 }
 
-//type UserInfo struct {
-//	Id        uint64 `json:"id"`
-//	Username  string `json:"username"`
-//	SayHello  string `json:"sayHello"`
-//	Password  string `json:"password"`
-//	CreatedAt string `json:"createdAt"`
-//	UpdatedAt string `json:"updatedAt"`
-//}
-//
-//type UserList struct {
-//	Lock  *sync.Mutex
-//	IdMap map[uint64]*UserInfo
-//}
-
-// Token represents a JSON web token.
 
